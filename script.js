5f0b09ae1717f0b2dc12a352c688d934a309ef8d
Alex Moiseev <coding-awm@mail.ru>
2020-06-29 18:14:09 +0600

    Сделал родителя

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+class First {
+    constructor() {
+
+    }
+    hello () {
+        console.log('Привет я метод родителя!');
+    }
+}
+
+const first = new First();
+first.hello();
