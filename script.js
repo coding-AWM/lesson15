00863f0f311b6653278bfa1d1fb063a096efe0d1
Alex Moiseev <coding-awm@mail.ru>
2020-06-29 18:23:07 +0600

    Сделал урок 15 второй наследование

--- script.js
+++ script.js
@@ -9,5 +9,17 @@ class First {
     }
 }
 
+class Second extends First{
+    constructor() {
+        super()
+    }
+    hello () {
+        super.hello()
+        console.log('А я наследуемый метод!');
+    }
+}
+
 const first = new First();
+const second = new Second();
 first.hello();
+second.hello();
